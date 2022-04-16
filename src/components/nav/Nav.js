97c9633c0e8d8e492521825f5fb232b9ac42ea20
satishmaurya4 <mauryasatish4488@gmail.com>
2022-04-16 16:02:33 +0530

    navbar design with basic logic completed

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,7 +1,5 @@
-import React, {useState} from 'react'
-import styles from "./Nav.module.css";
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import React, { useEffect, useState } from "react";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -10,61 +8,78 @@ import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 
-
-
-
 const Nav = () => {
+  const [isCloseOpen, setIsCloseOpen] = useState(false);
+  const [input, setInput] = useState("");
 
-    const [isCloseOpen, setIsCloseOpen] = useState(false);
-    const [input, setInput] = useState('');
-
-  
-
-    const closeToggleHandler = () => {
-        setIsCloseOpen(true);
+  const handleCloseIcon = () => {
+    if (!input) {
+      setIsCloseOpen(false);
+      console.log("inside if");
+    } else {
+      setIsCloseOpen(true);
     }
+  };
 
-    const inputHandler = (event) => {
-        setInput(event.target.value);   
-    }
-    
-    const clearInputHandler = () => {
-        setInput('');
-        setIsCloseOpen(false);
-    }
+  const handleClearInput = () => {
+    setInput("");
+    setIsCloseOpen(false);
+  };
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar style={{ background: "white" }}>
-          <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
-            <img
-              src="./images/SmartQ Logo.png" alt="logo"
-              style={{ height: "40px", objectFit: "contain" }}
-            />
-            <Paper
-              component="form"
-              sx={{
-                p: "2px 4px",
-                display: "flex",
-                alignItems: "center",
-                width: 400,
-                height: 30,
-                borderRadius: "10px"
-              }}
+  useEffect(() => {
+    handleCloseIcon();
+  }, [input]);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar style={{ background: "white" }}>
+        <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+          <img
+            src="./images/SmartQ Logo.png"
+            alt="logo"
+            style={{ height: "40px", objectFit: "contain" }}
+          />
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              width: 400,
+              height: 30,
+              borderRadius: "10px",
+            }}
+          >
+            <IconButton
+              disableRipple={true}
+              sx={{ p: "10px" }}
+              aria-label="search"
             >
-              <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-                <SearchIcon />
+              <SearchIcon />
+            </IconButton>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search Item"
+              inputProps={{ "aria-label": "search google maps" }}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            {isCloseOpen && (
+              <IconButton
+                disableRipple={true}
+                sx={{ p: "10px" }}
+                aria-label="clear search"
+                style={{ color: "tomato" }}
+                onClick={handleClearInput}
+              >
+                <CloseOutlinedIcon />
               </IconButton>
-              <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder="Search Item"
-                inputProps={{ "aria-label": "search google maps" }}
-              />
-            </Paper>
-          </Toolbar>
-        </AppBar>
-      </Box>
-  )
-}
+            )}
+          </Paper>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default Nav
+export default Nav;
